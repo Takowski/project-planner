3c71cf8a93bccd3a160d0b04697a8040584584ad
Robpiot <robin.piot@outlook.com>
2023-10-03 11:39:35 +0200

    filtering

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-
+// creating new items
 const cardForm = document.getElementById('card-form');
 const todoContainer = document.getElementById('todo-container');
 const doingContainer = document.getElementById('doing-container');
@@ -6,34 +6,75 @@ const doneContainer = document.getElementById('done-container');
 
 cardForm.addEventListener('submit', event => {
     event.preventDefault();
-
+    
     const formData = new FormData(cardForm);
     const card = Object.fromEntries(formData.entries());
-
+    
     const cardHTML = `
-        <div class="card">
-            <h2>${card.title}</h2>
-            <p>${card.description}</p>
-            <p>Due Date: ${card.dueDate}</p>
-            <p>time left: 'lol'</p>
-            <p>status: ${card.status}</p>
-            
-        </div>
+    <div class="card">
+    <h2>${card.title}</h2>
+    <p>${card.description}</p>
+    <p>Due Date: ${card.dueDate}</p>
+    <p>time left: 'lol'</p>
+    <p>status: ${card.status}</p>
+    
+    </div>
     `;
-
+    
     switch (card.status) {
         case 'todo':
-            todoContainer.insertAdjacentHTML('beforeend', cardHTML);
-            break;
+        todoContainer.insertAdjacentHTML('beforeend', cardHTML);
+        break;
         case 'doing':
-            doingContainer.insertAdjacentHTML('beforeend', cardHTML);
-            break;
+        doingContainer.insertAdjacentHTML('beforeend', cardHTML);
+        break;
         case 'done':
-            doneContainer.insertAdjacentHTML('beforeend', cardHTML);
-            break;
+        doneContainer.insertAdjacentHTML('beforeend', cardHTML);
+        break;
         default:
-            break;
+        break;
     }
-
+    
     cardForm.reset();
 });
+
+
+// Sorting items by name & value 
+const sortingList = document.getElementById('sortingStatus');
+const sections = document.querySelectorAll("section ");
+
+// getting change
+
+sortingList.addEventListener("change", () => {
+    const pickedValue = sortingList.value;
+    
+    // show only the category user picked up 
+    
+    sections.forEach(function() {
+        switch (pickedValue) {
+            case  'all' :
+            document.getElementById('todo-container').style.display = "block";
+            document.getElementById('doing-container').style.display = "block";
+            document.getElementById('done-container').style.display = "block";
+            break;
+            case 'todo' : 
+            document.getElementById('todo-container').style.display = "block";
+            document.getElementById('doing-container').style.display = "none";
+            document.getElementById('done-container').style.display = "none";
+            break;
+            case 'doing' : 
+            document.getElementById('todo-container').style.display = "none";
+            document.getElementById('doing-container').style.display = "block";
+            document.getElementById('done-container').style.display = "none";
+            break;
+            case 'done' : 
+            document.getElementById('todo-container').style.display = "none";
+            document.getElementById('doing-container').style.display = "none";
+            document.getElementById('done-container').style.display = "block";
+            break;
+            
+            
+        }
+        
+    })
+})
